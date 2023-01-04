964fcb47a560dec19eb68750f99b381c8531f6bd
Igors Koptevs <igors.koptevs@gmail.com>
2023-01-04 13:41:14 +0200

    added dark mode support

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import * as React from "react"
-import Container from "@mui/material/Container"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Box } from "@mui/system"
+import { IconButton } from "@mui/material"
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
 import Typography from "@mui/material/Typography"
-import Box from "@mui/material/Box"
 
-import Button from "@mui/material/Button"
-import DeleteIcon from "@mui/icons-material/Delete"
-import SendIcon from "@mui/icons-material/Send"
-import Stack from "@mui/material/Stack"
+import { ColorModeContext, useMode, colorTokens } from "./theme"
 
 export default function App() {
+    const [theme, colorMode] = useMode()
+    const colors = colorTokens(theme.palette.mode)
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ my: 4 }}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    React with Vite & MUI
-                </Typography>
-
-                <Stack direction="row" spacing={2}>
-                    <Button variant="outlined" startIcon={<DeleteIcon />}>
-                        Delete
-                    </Button>
-                    <Button variant="contained" endIcon={<SendIcon />}>
-                        Send
-                    </Button>
-                </Stack>
-            </Box>
-        </Container>
+        <>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <div className="app">
+                        <main className="content">
+                            <IconButton onClick={colorMode.toggleColorMode}>
+                                {theme.palette.mode === "dark" ? (
+                                    <DarkModeOutlinedIcon />
+                                ) : (
+                                    <LightModeOutlinedIcon />
+                                )}
+                            </IconButton>
+                            <Typography variant="h1">
+                                Heading 1 - color comes from the theme settings
+                                for headings
+                            </Typography>
+                            <Box
+                                sx={{
+                                    fontSize: "18px",
+                                    fontWeight: "bold",
+                                    color: colors.greenAccent[300],
+                                }}
+                            >
+                                Box - inline styles with sx property - color
+                                imported from colorTokens palette inside
+                                theme.js file
+                            </Box>
+                        </main>
+                    </div>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        </>
     )
 }
